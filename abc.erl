22ceecdeeefcffa55d7790894b8dce5e5ab2cb80
souravghatak <sourav.ghatak@ee.co.uk>
2017-10-09 18:54:46 +0100

    Update abc.erl

--- abc.erl
+++ abc.erl
@@ -7,6 +7,5 @@ io:format("My name is : ~p~n", ["Sourav Ghatak"]),
 
 io:format("Everything is gonna be : ~p~n", ["alright"]).
 
-io:format("My first commit for branch: ~p~n", ["Test-2"]),
-io:format("My second commit for branch: ~p~n", ["Test-2"]),
+
 
