2033d1c571bb19a8e5508fc0e080f8f0a89e8cd4
souravghatak <sourav.ghatak@ee.co.uk>
2017-10-09 19:32:14 +0100

    Update abc.erl

--- abc.erl
+++ abc.erl
@@ -5,7 +5,7 @@ hello() ->
 
 io:format("My name is : ~p~n", ["Sourav Ghatak"]),
 
-io:format("Everything is gonna be : ~p~n", ["alright"]).
+io:format("Everything is gonna be : ~p~n", ["good"]).
 
 
 
