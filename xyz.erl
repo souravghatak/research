07d42752d3aa519c249ed9247fff74a62f589088
souravghatak <sourav.ghatak@ee.co.uk>
2017-10-20 12:03:36 +0100

    Upp

--- xyz.erl
+++ xyz.erl
@@ -4,5 +4,5 @@
 hello() ->
 
 io:format("My name is : ~p~n", ["Sourav, Whats up"]),
-uuuiiiiiio:format("My first commit for branch: ~p~n", ["Test-26"]),
+yyyyuuuiiiiiio:format("My first commit for branch: ~p~n", ["Test-26"]),
 
